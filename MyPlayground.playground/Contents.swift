7f577b1e1e64a8bb74e27309695e156f67695b87
wyn <k022298@gmail.com>
2020-02-16 22:49:52 +0800

    learnning how to use var let and print

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var studentA = "Tommy"
+var studentB = "Mary"
+let shoseSizeA = 26
+let shoseSizeB = 24
+
+print("No.\(shoseSizeA) is \(studentA)'s shose size")
+print("And no.\(shoseSizeB) is \(studentB)'s shose size")
