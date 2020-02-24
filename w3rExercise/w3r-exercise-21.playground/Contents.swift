bd4421439b0f1539bc82e434befd6798c9c5a3b3
wyn <k022298@gmail.com>
2020-02-24 14:02:00 +0800

    updated exercise 21

--- w3rExercise/w3r-exercise-21.playground/Contents.swift
+++ w3rExercise/w3r-exercise-21.playground/Contents.swift
@@ -1,16 +1,17 @@
 //Write a Swift program to create a string made of every other char starting with the first from a given string. So "Python" will return "Pto"
+import Foundation
 
-func new_string(_ input: String) -> String {
-    var str1 = ""
-    let chars = Array(input)
-    
-    for (index, char) in chars.enumerated() {
-        if index % 2 == 0 {
-            str1.append(char)
+func rebuildStr(_ str:String)->String{
+    var outputStr = ""
+    let strCha = Array(str)
+    for (index,cha) in strCha.enumerated(){
+        if index % 2 == 0{
+            outputStr.append(cha)
         }
     }
-    return str1
+    return outputStr
 }
-print(new_string("Python"))
-print(new_string("Swift"))
-print(new_string("Ab"))
+print(rebuildStr("Hello"))
+print(rebuildStr("Python"))
+
+
